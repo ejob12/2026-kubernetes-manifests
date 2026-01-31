ddad7c54d716e92725be854bdf51c22d800cc9cc
Ejob Jason <ejobjason4@gmail.com>
2026-01-31 16:29:45 +0100

    edited two files main.tf and backend.tf

--- 01-eks-cluster/backend.tf
+++ 01-eks-cluster/backend.tf
@@ -3,9 +3,9 @@
 
 terraform {
   backend "s3" {
-    bucket         = "2026-state-ejob"
-    key            = "eks/liontech-dev/terraform.tfstate"
-    region         = "us-east-1"
+    bucket = "2026-state-ejob"
+    key    = "eks/liontech-dev/terraform.tfstate"
+    region = "us-east-1"
     # encrypt        = true
     # dynamodb_table = "terraform-locks"
   }
--- 01-eks-cluster/main.tf
+++ 01-eks-cluster/main.tf
@@ -18,8 +18,8 @@ resource "aws_vpc" "eks_vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name                                           = "${var.cluster_name}-vpc"
-    "kubernetes.io/cluster/${var.cluster_name}"   = "shared"
+    Name                                        = "${var.cluster_name}-vpc"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
   }
 }
 
@@ -40,9 +40,9 @@ resource "aws_subnet" "public_subnet_1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name                                           = "${var.cluster_name}-public-subnet-1"
-    "kubernetes.io/cluster/${var.cluster_name}"   = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+    Name                                        = "${var.cluster_name}-public-subnet-1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
 
@@ -53,9 +53,9 @@ resource "aws_subnet" "public_subnet_2" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name                                           = "${var.cluster_name}-public-subnet-2"
-    "kubernetes.io/cluster/${var.cluster_name}"   = "shared"
-    "kubernetes.io/role/elb"                      = "1"
+    Name                                        = "${var.cluster_name}-public-subnet-2"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
   }
 }
 
@@ -66,9 +66,9 @@ resource "aws_subnet" "private_subnet_1" {
   availability_zone = "${var.aws_region}a"
 
   tags = {
-    Name                                           = "${var.cluster_name}-private-subnet-1"
-    "kubernetes.io/cluster/${var.cluster_name}"   = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    Name                                        = "${var.cluster_name}-private-subnet-1"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
 
@@ -78,9 +78,9 @@ resource "aws_subnet" "private_subnet_2" {
   availability_zone = "${var.aws_region}b"
 
   tags = {
-    Name                                           = "${var.cluster_name}-private-subnet-2"
-    "kubernetes.io/cluster/${var.cluster_name}"   = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    Name                                        = "${var.cluster_name}-private-subnet-2"
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
   }
 }
 
@@ -133,8 +133,8 @@ resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.eks_vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    gateway_id      = aws_internet_gateway.eks_igw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
   }
 
   tags = {
@@ -157,8 +157,8 @@ resource "aws_route_table" "private_rt_1" {
   vpc_id = aws_vpc.eks_vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat_gateway_1.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_1.id
   }
 
   tags = {
@@ -170,8 +170,8 @@ resource "aws_route_table" "private_rt_2" {
   vpc_id = aws_vpc.eks_vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat_gateway_2.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway_2.id
   }
 
   tags = {
@@ -221,10 +221,10 @@ resource "aws_security_group" "eks_node_sg" {
   vpc_id      = aws_vpc.eks_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    self        = true
+    from_port = 0
+    to_port   = 65535
+    protocol  = "tcp"
+    self      = true
   }
 
   ingress {
@@ -281,15 +281,12 @@ resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
 
 # EKS Cluster
 resource "aws_eks_cluster" "eks_cluster" {
-  name            = var.cluster_name
-  version         = var.kubernetes_version
-  role_arn        = aws_iam_role.eks_cluster_role.arn
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids              = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-    security_group_ids      = [aws_security_group.eks_cluster_sg.id]
-    endpoint_private_access = true
-    endpoint_public_access  = true
+    subnet_ids         = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
 
   depends_on = [
@@ -300,68 +297,4 @@ resource "aws_eks_cluster" "eks_cluster" {
   tags = {
     Name = var.cluster_name
   }
-}
-
-# IAM Role for EKS Node Group
-resource "aws_iam_role" "eks_node_role" {
-  name = "${var.cluster_name}-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.cluster_name}-node-role"
-  }
-}
-
-# Attach the required policies to the node role
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-# EKS Node Group
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
-
-  scaling_config {
-    desired_size = var.desired_size
-    max_size     = var.max_size
-    min_size     = var.min_size
-  }
-
-  instance_types = var.instance_types
-
-  tags = {
-    Name = "${var.cluster_name}-node-group"
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_worker_node_policy,
-    aws_iam_role_policy_attachment.eks_cni_policy,
-    aws_iam_role_policy_attachment.eks_container_registry_policy
-  ]
 }
